b6071cc01f17d90c5c90d572784d412e9e1e361a
Andreas Hupfer <andreas@hupfer.ch>
2018-09-05 13:00:18 +0200

    add blueprints vor main and auth

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
-from app import db
-
+from app import db, login_manager
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
+
+    @login_manager.user_loader
+    def load_user(self, user_id):
+        return User.get(user_id)
 
     def __repr__(self):
         return '<User %r>' % self.username
